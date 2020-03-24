684cfe711960787353b26ba14eb9ac86056b20e3
Cody Martin <codemart10@gmail.com>
2020-03-24 14:24:12 -0400

    simple name enter function

--- game/engine.py
+++ game/engine.py
@@ -1,7 +1,27 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, redirect, url_for, request, session)
 
 bp = Blueprint('engine', __name__)
 
-@bp.route('/')
-def engine():
-    return render_template('index.html')
+#@bp.route('/index')
+#def engine():
+#    name = session.get(username)
+#    return render_template('index.html')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def user():
+    if request.method == 'POST':
+        username = request.form['username']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+
+        if error is None:
+            session.clear()
+            session['username'] = username
+            return render_template('index.html', username=username)
+
+        flash(error)
+
+    return render_template('username.html')
--- game/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-  username NOT NULL,
-);
