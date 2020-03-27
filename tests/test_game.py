bcec0d0fff4be0ec1229f829d70d62c0a746d0f6
Cody Martin <codemart10@gmail.com>
2020-03-27 15:33:11 -0400

    last part not finished

--- tests/test_game.py
+++ tests/test_game.py
@@ -5,9 +5,10 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-def test_index(client):
+def test_username(client):
     response = client.get('/')
     client.post('/', data={'username': 'test'})
 
-
-## write more testsssss
+def test_rooms(client):
+    response = client.get('engine/Tank')
+    assert 'http://localhost/engine/Tank' == response.headers['Location']
